2defbf7422572d97cfa29e10282ae7108f4c964d
Phil <matundaphyl@gmail.com>
2020-05-27 14:47:00 +0300

    gigasecond

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,3 +1,4 @@
+/*
 const COLORS = [
   "black",
   "brown",
@@ -14,3 +15,44 @@ const COLORS = [
 export const decodedValue = (colors) => {
   return Number(`${COLORS.indexOf(colors[0])}${COLORS.indexOf(colors[1])}`);
 };
+
+
+*****************************************************************************************
+**************************************************************************
+*****************************************************************
+
+
+export const decodedValue = (array) => {
+  let resultArray = [];
+
+  array.slice(0, 2).forEach((element) => {
+    let code = resistorColorCodes.indexOf(element);
+    resultArray.push(code);
+  });
+  return Number(resultArray.join(""));
+};
+
+const resistorColorCodes = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
+
+
+*****************************************************************************************
+**************************************************************************
+*****************************************************************
+*/
+
+export const decodedValue = (arr) => {
+  return Number('' + COLORS.indexOf(arr[0]) + COLORS.indexOf(arr[1]));
+};
+
+const COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
