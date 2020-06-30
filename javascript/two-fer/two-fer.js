b64ee188599ef2198adbbd03e874139ea5c422ba
Phil <matundaphyl@gmail.com>
2020-06-30 15:01:43 +0300

    modified two-fer, leap and space-age

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -3,6 +3,13 @@
 // convenience to get you started writing code faster.
 //
 
+/*
 export const twoFer = (name = 'you') => {
   return `One for ${name}, one for me.`
 };
+
+export const twoFer = (name) => {
+  return !name ? "One for you, one for me." : `One for ${name}, one for me.`;
+};
+
+*/
